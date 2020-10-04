65d518be3a049a294dcd6d5fc7d360a2f19edf7d
jndeng <dengjn@zju.edu.cn>
2020-10-05 03:34:57 +0800

    First commit

---
+++ codes/models/__init__.py
@@ -0,0 +1,26 @@
+from .vsr_model import VSRModel
+from .vsrgan_model import VSRGANModel
+
+
+# register vsr model
+vsr_model_lst = [
+    'frvsr',
+]
+
+# register vsrgan model
+vsrgan_model_lst = [
+    'tecogan',
+]
+
+
+def define_model(opt):
+    if opt['model']['name'].lower() in vsr_model_lst:
+        model = VSRModel(opt)
+
+    elif opt['model']['name'].lower() in vsrgan_model_lst:
+        model = VSRGANModel(opt)
+
+    else:
+        raise ValueError('Unrecognized model: {}'.format(opt['model']['name']))
+
+    return model
